267967ca2b702b69c805875e8f72b4c02b52f3cc
Emanuel Santos <emanuelsantos9627@gmail.com>
2021-08-02 14:40:21 -0300

    center-button

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,37 +12,44 @@ Color kPrimaryColor = Color.fromRGBO(152, 140, 130, 1);
 
 class _HomeScreenState extends State<HomeScreen> {
   final math.Random _random = math.Random();
-  SystemUiOverlayStyle _currentStyle = SystemUiOverlayStyle.light;
-
-  void _randomColor() {}
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Random Colors'),
-          elevation: 0,
-          backgroundColor: kPrimaryColor,
+      appBar: AppBar(
+        title: Text(
+          'Random Colors',
+          style: GoogleFonts.lato(),
         ),
+        elevation: 0,
         backgroundColor: kPrimaryColor,
-        body: AnnotatedRegion<SystemUiOverlayStyle>(
-          value: _currentStyle,
-          child: Center(
-            child: ElevatedButton(
-              onPressed: () {
-                final Color color = Color.fromRGBO(
-                  _random.nextInt(255),
-                  _random.nextInt(255),
-                  _random.nextInt(255),
-                  1,
-                );
-                setState(() {
-                  kPrimaryColor = color;
-                });
-              },
-              child: const Text('Random Color'),
-            ),
+      ),
+      backgroundColor: kPrimaryColor,
+      body: Center(
+          child: Container(
+        width: 70,
+        height: 70,
+        decoration: BoxDecoration(
+          color: Colors.lightBlue,
+          shape: BoxShape.circle,
+        ),
+        child: TextButton(
+          onPressed: () {
+            final Color color = Color.fromRGBO(
+              _random.nextInt(255),
+              _random.nextInt(255),
+              _random.nextInt(255),
+              1,
+            );
+            setState(() {
+              kPrimaryColor = color;
+            });
+          },
+          child: Text(
+            'R',
+            style: GoogleFonts.montserrat(color: Colors.white, fontSize: 24),
           ),
-        ));
+        ),
+      )),
+    );
   }
 }
